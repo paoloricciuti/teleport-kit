c45a5920d7961ea0b8ceb1639adf9dbcd9151922
paoloricciuti <ricciutipaolo@gmail.com>
2024-05-15 00:37:02 +0200

    chore: move tests to playground

--- playwright.config.ts
+++ playwright.config.ts
@@ -2,7 +2,7 @@ import type { PlaywrightTestConfig } from '@playwright/test';
 
 const config: PlaywrightTestConfig = {
 	webServer: {
-		command: 'pnpm package && pnpm run build && pnpm run preview',
+		command: 'pnpm package && pnpm run build:playground && pnpm run preview:playground',
 		port: 4173,
 		stderr: 'pipe',
 		stdout: 'pipe'
